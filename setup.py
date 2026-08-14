820053f600e450d34572c11d1a0ad929d682dc4b
Perkybeet <yago.lopez.adeje@gmail.com>
2026-08-14 13:16:55 +0100

    v1.4.0: full panel management, deployment history, live metrics, webhooks, notifications

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="wasm-cli",
-    version="1.3.0",
+    version="1.4.0",
     description="Web App System Management - Deploy and manage web applications on Linux servers",
     author="Yago López Prado",
     author_email="yago.lopez.adeje@gmail.com",
